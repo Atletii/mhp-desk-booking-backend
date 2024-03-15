9a7cc5dd03e2f33b4a2b01424edd5281baf92f86
Andrei Galitianu <andrei@galitia.nu>
2024-03-15 15:31:06 +0200

    initial commit

---
+++ src/main/java/com/atletii/mhpdeskbookingbackend/MhpDeskBookingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.atletii.mhpdeskbookingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MhpDeskBookingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MhpDeskBookingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atletii/mhpdeskbookingbackend/MhpDeskBookingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atletii.mhpdeskbookingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MhpDeskBookingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
