171f019cfbf486898f017539aa6da49e8a9fceb1
Oprișor Raul-Alexandru <oprisorraul@gmail.com>
2024-03-16 23:25:04 +0200

    added list of rooms

--- src/main/java/com/atletii/mhpdeskbookingbackend/rooms/api/dto/RoomDto.java
+++ src/main/java/com/atletii/mhpdeskbookingbackend/rooms/api/dto/RoomDto.java
@@ -1,12 +1,15 @@
 package com.atletii.mhpdeskbookingbackend.rooms.api.dto;
 
 import com.atletii.mhpdeskbookingbackend.common.api.BaseEntityDto;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class RoomDto extends BaseEntityDto {
     private String name;
+    private Integer mapId;
     private Integer nrPlaces;
 }
--- src/main/java/com/atletii/mhpdeskbookingbackend/rooms/service/model/Room.java
+++ src/main/java/com/atletii/mhpdeskbookingbackend/rooms/service/model/Room.java
@@ -1,12 +1,15 @@
 package com.atletii.mhpdeskbookingbackend.rooms.service.model;
 
 import com.atletii.mhpdeskbookingbackend.common.service.BaseEntityModel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class Room extends BaseEntityModel {
     private String name;
     private Integer nrPlaces;
+    private Integer mapId;
 }
